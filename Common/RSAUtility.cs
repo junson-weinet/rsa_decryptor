6a6fd3dbdfb6446852fe78377ac2b63ab7e48d7d
zarky <m2jawer@gmail.com>
2022-03-30 11:59:44 +0800

    create

---
+++ Common/RSAUtility.cs
@@ -0,0 +1,63 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace rsa_decryptor.Common
+{
+    public static class RSAUtility
+    {
+        /// <summary>
+        /// base64 public key -> encrypt content
+        /// </summary>
+        /// <param name="publicKey">must base64 string</param>
+        /// <param name="content">encrypt content</param>
+        /// <returns></returns>
+        public static string Encrypt(string publicKey, string content)
+        {
+            Chilkat.SshKey puttyKey = new Chilkat.SshKey();
+            if (puttyKey.FromOpenSshPublicKey(publicKey))
+            {
+                string xmlPublicKey = puttyKey.ToXml();
+
+                string encryptedContent = string.Empty;
+                using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+                {
+                    rsa.FromXmlString(xmlPublicKey);
+                    byte[] encryptedData = rsa.Encrypt(Encoding.UTF8.GetBytes(content), false);
+                    encryptedContent = Convert.ToBase64String(encryptedData);
+                }
+                return encryptedContent;
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// base64 private key -> decrypt content
+        /// </summary>
+        /// <param name="privateKey">must base64 string</param>
+        /// <param name="content">decrypt content</param>
+        /// <returns></returns>
+        public static string Decrypt(string privateKey, string content)
+        {
+            Chilkat.SshKey puttyKey = new Chilkat.SshKey();
+            if (puttyKey.FromOpenSshPrivateKey(privateKey))
+            {
+                string xmlPrivateKey = puttyKey.ToXml();
+                string decryptedContent = string.Empty;
+                using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+                {
+                    rsa.FromXmlString(xmlPrivateKey);
+                    byte[] decryptedData = rsa.Decrypt(Convert.FromBase64String(content), false);
+                    decryptedContent = Encoding.UTF8.GetString(decryptedData);
+                }
+                return decryptedContent;
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//execute sample
+//rsa_decryptor r C:\Users\admin\id_rsa ba8QutmxuSEyx4g+nDDybIqtUWzXwaGgeuAQ4NWuLCk9V5GlQCF64EhPwbKEcCo+8h3ZJN2Ke74fRpx79fYMajJ2adZzm016EJU0tLPX5A3/ITIOH/PDxHulI9mVCzWwv7pNOb2K86CdHZOnYrIfqbQyw8EIQBJ9sCyA54SVDpbpjlEJPNLA3WKOVgg5WGDvCPAOlIIEyH1bBm0FieKH07TuvlCoAnESpQDun7QHmE5ksaIDed+x9NXkG4QMfOhRasJNTHDpOHdCIgYWRxBWqVQ0ttLHdmQZGBzU6TwQ7OJbqmA6bZeE8P6KkaOFqpjuzEwceMJm/Ja/cibK2UNoqw==
+using rsa_decryptor.Common;
+
+if (args[0] == "r")
+{
+    string privateKey = File.ReadAllText(args[1]);
+    Console.WriteLine(RSAUtility.Decrypt(privateKey, args[2]));
+}
